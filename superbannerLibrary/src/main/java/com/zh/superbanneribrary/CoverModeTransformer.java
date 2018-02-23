1c53804e09f3c952adedbe08a571294864e71daa
zhouwenbin <345059887@qq.com>
2018-02-23 11:26:41 +0800

    修改部分bug

--- superbannerLibrary/src/main/java/com/zh/superbanneribrary/CoverModeTransformer.java
+++ superbannerLibrary/src/main/java/com/zh/superbanneribrary/CoverModeTransformer.java
@@ -1,82 +1,60 @@
 package com.zh.superbanneribrary;
 
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
-
-/**
- * Created by zhouwei on 17/8/20.
- */
 
 public class CoverModeTransformer implements ViewPager.PageTransformer {
 
     private final String TAG = CoverModeTransformer.class.getCanonicalName();
-    private float reduceX = 0.0f;
-    private float itemWidth = 0;
-    private float offsetPosition = 0f;
-    private float mScaleMax = 1.0f;
-    private float mScaleMin = 0.9f;
     private ViewPager mViewPager;
-
-    private static final float MIN_SCALE = 0.70f;
     private float MIN_ALPHA = 0.5f;
 
     public CoverModeTransformer(ViewPager pager, float MIN_ALPHA) {
         mViewPager = pager;
+        if (MIN_ALPHA < 0.5f) {
+            MIN_ALPHA = 0.5f;
+        }
         this.MIN_ALPHA = MIN_ALPHA;
     }
 
     @Override
     public void transformPage(View view, float position) {
-        if (position < -1 || position > 1) {
-            view.setAlpha(MIN_ALPHA);
-            view.setScaleX(MIN_SCALE);
-            view.setScaleY(MIN_SCALE);
-        } else if (position <= 1) { // [-1,1]
-            float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
-            if (position < 0) {
-                float scaleX = 1 + 0.3f * position;
-                view.setScaleX(scaleX);
-                view.setScaleY(scaleX);
-            } else {
-                float scaleX = 1 - 0.3f * position;
-                view.setScaleX(scaleX);
-                view.setScaleY(scaleX);
-            }
-            view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
-        }
+        float mScaleMax = 1.0f;
+        float mScaleMin = 0.9f;
 
-
-        if (offsetPosition == 0f) {
-            float paddingLeft = mViewPager.getPaddingLeft();
-            float paddingRight = mViewPager.getPaddingRight();
-            float width = mViewPager.getMeasuredWidth();
-            offsetPosition = paddingLeft / (width - paddingLeft - paddingRight);
-        }
+        float paddingLeft = mViewPager.getPaddingLeft();
+        float paddingRight = mViewPager.getPaddingRight();
+        float width = mViewPager.getMeasuredWidth();
+        float offsetPosition = paddingLeft / (width - paddingLeft - paddingRight);
+        Log.i(TAG, paddingLeft + "***" + paddingRight + "*****" + width);
         float currentPos = position - offsetPosition;
-        if (itemWidth == 0) {
-            itemWidth = view.getWidth();
-            //由于左右边的缩小而减小的x的大小的一半
-            reduceX = (2.0f - mScaleMax - mScaleMin) * itemWidth / 2.0f;
-        }
+        float itemWidth = view.getWidth();
+        //由于左右边的缩小而减小的x的大小的一半
+        float reduceX = (2.0f - mScaleMax - mScaleMin) * itemWidth / 2.0f;
         if (currentPos <= -1.0f) {
             view.setTranslationX(reduceX);
             view.setScaleX(mScaleMin);
             view.setScaleY(mScaleMin);
+            view.setAlpha(MIN_ALPHA);
         } else if (currentPos <= 1.0) {
             float scale = (mScaleMax - mScaleMin) * Math.abs(1.0f - Math.abs(currentPos));
             float translationX = currentPos * -reduceX;
-            if (currentPos <= -0.5) {//两个view中间的临界，这时两个view在同一层，左侧View需要往X轴正方向移动覆盖的值()
-                view.setTranslationX(translationX +  Math.abs(Math.abs(currentPos) - 0.5f) / 0.5f);
+            if (currentPos <= -0.5) {  //两个view中间的临界，这时两个view在同一层，左侧View需要往X轴正方向移动覆盖的值()
+                view.setTranslationX(translationX + Math.abs(Math.abs(currentPos) - 0.5f) / 0.5f);
             } else if (currentPos <= 0.0f) {
                 view.setTranslationX(translationX);
-            } else if (currentPos >= 0.5) {//两个view中间的临界，这时两个view在同一层
-                view.setTranslationX(translationX -  Math.abs(Math.abs(currentPos) - 0.5f) / 0.5f);
+            } else if (currentPos >= 0.5) {  //两个view中间的临界，这时两个view在同一层
+                view.setTranslationX(translationX - Math.abs(Math.abs(currentPos) - 0.5f) / 0.5f);
             } else {
                 view.setTranslationX(translationX);
             }
             view.setScaleX(scale + mScaleMin);
             view.setScaleY(scale + mScaleMin);
+            float scaleFactor = Math.max(mScaleMin, 1 - Math.abs(position));
+            view.setAlpha(MIN_ALPHA + (scaleFactor - mScaleMin) / (1 - mScaleMin) * (1 - MIN_ALPHA));
         } else {
+            view.setAlpha(MIN_ALPHA);
             view.setScaleX(mScaleMin);
             view.setScaleY(mScaleMin);
             view.setTranslationX(-reduceX);
